ed5faa6911508f5f8504a8d646ae9ad4c4ca841d
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-07-09 22:05:02 +0900

    時刻表示

--- Takachon/iOS-client/OneDayInternshipApp/Message.swift
+++ Takachon/iOS-client/OneDayInternshipApp/Message.swift
@@ -12,13 +12,15 @@ class Message {
     let identifier: Int?
     let body: String!
     // 1-1 時刻データを扱うためのプロパティ追加
+    let dateString: String!
     
     // 1-2 ユーザ名データを扱うためのプロパティ追加
     
-    init(identifier: Int?, body: String) {
+    init(identifier: Int?, body: String, dateString: String) {
         self.identifier = identifier
         self.body = body
         // 1-1 時刻データを用いて初期化
+        self.dateString = dateString
         
         // 1-2 ユーザ名データを用いて初期化
     }
